b8f3a8dd1728ee673d44a4b68ac90093b2352c29
Name <vbrfqlj@mail.ru>
2025-09-21 13:17:49 +0300

    Задание 29.7 дальше

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,68 @@
+// Запросим возраст пользователя и сохраним в переменную
+// let userName = prompt("Пожалуйста, введите ваше имя");
+// alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`)
+let session =  new Map();
+/*
+* Функция для фильтрации контента
+*
+* */
+function filterContent(){
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if(!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */  .toLowerCase())){
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+/*
+* Функция для проверки и сохранения  данных пользователя
+* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
+*
+* */
+/*
+* Вывод данных сессии в консоль
+*
+* */
+let sessionLog = function logSession() {
+    for (let result of session){
+        console.log(result)
+    }
+}
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+*
+* */
+function handleSession(){
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+/*
+* Проверка возраста пользователя
+*
+* */
+function checkAge(){
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if(session.get("age") >= 18){
+        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+    }
+    else{
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+/*
+* Всплывающее окно будет показано по таймауту
+*
+* */
+setTimeout(() =>
+        alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+    60000);
